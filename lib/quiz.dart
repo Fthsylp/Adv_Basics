5675059e272131b702e688e2ccd4b65fbe9b8031
Fatih Soyalp <fth.sylp@gmail.com>
2025-03-01 11:47:37 +0300

    Quiz questions added.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,10 @@
+import 'package:adv_basics/questions_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
-  
+
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -11,8 +12,22 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+
+  var activeScreen = 'start-screen';
+
+  void switchScreen(){
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
+
+    final screenWidget = activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen();
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -26,7 +41,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomCenter,
             ),
           ),
-          child: const StartScreen(),
+          child: screenWidget,
         ),
       ),
     );
