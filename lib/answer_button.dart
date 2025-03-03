d8b77f904df74747bae2d84cbb09b0ac35f612fa
Fatih Soyalp <fth.sylp@gmail.com>
2025-03-03 12:56:59 +0300

    Add QuestionsScreen and AnswerButton components

---
+++ lib/answer_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(1, 39, 160, 216),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
