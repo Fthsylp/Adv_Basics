9b73c31da87b42ed599ed3c11ea253a2bc3ba86c
Fatih Soyalp <fth.sylp@gmail.com>
2025-03-17 11:21:39 +0300

    Add results summary feature with detailed answer feedback

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,7 +2,6 @@ import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:adv_basics/results_screen.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key, required this.onSelectedAnswer});
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,36 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
 
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_idex': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions =
+        summaryData.where((data) {
+          return data['correct_answer'] == data['user_answer'];
+        }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -14,10 +38,13 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('You answered X out of Y questions correctly!'),
-            SizedBox(height: 20),
-            Text('List of answers and questions...'),
-            SizedBox(height: 20),
+            Text(
+              'You answered ${numTotalQuestions} out of ${numCorrectQuestions} questions correctly!',
+            ),
+            const SizedBox(height: 20),
+            QuestionsSummary(summaryData),
+            const Text('List of answers and questions...'),
+            const SizedBox(height: 20),
             ElevatedButton(onPressed: () {}, child: Text('Restart Quiz')),
           ],
         ),
